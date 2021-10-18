552cc7cf3ad354f3ef01a9bef1a80682b8de79c7
cuongtien00 <cuonghiep000@gmail.com>
2021-10-18 14:28:47 +0700

    add Class CustomerManager

--- src/controller/CustomerManager.java
+++ src/controller/CustomerManager.java
@@ -1,4 +1,22 @@
 package controller;
 
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerManager {
+    private List<Customer> customerList;
+
+    public CustomerManager() {
+        customerList = new ArrayList<>();
+    }
+    public void inputInfo(){
+
+    }
+    public void showList(){
+        for (Customer a:customerList) {
+            System.out.println(a);
+        }
+    }
 }
