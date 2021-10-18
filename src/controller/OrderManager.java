f06fb715d80500c434ab915f997392342742fd38
cuongtien00 <cuonghiep000@gmail.com>
2021-10-18 14:34:08 +0700

    add OrderManager

--- src/controller/OrderManager.java
+++ src/controller/OrderManager.java
@@ -1,4 +1,35 @@
 package controller;
 
+import model.MilkTea;
+import model.Order;
+
+import java.util.List;
+
 public class OrderManager {
+    private List<Order>orderList;
+
+    public OrderManager() {
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+    public void addNewOrder(Order order){
+        orderList.add(order);
+    }
+    public void editOrder(int index, Order order){
+        orderList.set(index,order);
+    }
+    public void removeOrder(Order order){
+        orderList.remove(order);
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+    public void showList(){
+        for (Order a:orderList) {
+            System.out.println(a);
+        }
+    }
 }
